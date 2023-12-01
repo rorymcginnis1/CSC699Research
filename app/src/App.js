0bd6f8042ef77e850092e9ca07c4409cf67e4604
Kishan <krp007kishanrpatel@gmail.com>
2023-12-01 03:23:11 -0800

    Added parallel coordinates plot on frontend

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ParallelCoordinatesPlot from './components/ParallelCoordinatesPlot';
+import { data } from './data';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <ParallelCoordinatesPlot
+                data={data}
+                width={1000}
+                height={400}
+                VARIABLES={21}
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ app/src/components/ParallelCoordinatesPlot.js
@@ -0,0 +1,72 @@
+import * as d3 from 'd3';
+import AxisVertical from './AxisVertical';
+
+const MARGIN = { top: 60, right: 40, bottom: 30, left: 40 };
+
+const ParallelCoordinatePlot = ({data, width, height, VARIABLES}) => {
+  const boundsWidth = width - MARGIN.right - MARGIN.left;
+  const boundsHeight = height - MARGIN.top - MARGIN.bottom;
+  const variables = Array.from({length: VARIABLES}, (_, index) => index); //Creates an array from 0 to VARIABLES
+
+  // Compute a xScale: spread all Y axis along the chart width
+  const xScale = d3
+    .scalePoint()
+    .range([0, boundsWidth])
+    .domain(variables)
+    .padding(0);
+
+  // Compute the yScales: 1 scale per variable
+  let yScales = {};
+  variables.forEach((variable) => {
+    yScales[variable] = d3
+      .scaleLinear()
+      .range([boundsHeight, 0])
+      .domain([0, 1]);
+  });
+
+  // Compute lines
+  const lineGenerator = d3.line();
+
+  const allLines = data.map((series, i) => {
+    const allCoordinates = variables.map((variable) => {
+      const yScale = yScales[variable];
+      const x = xScale(variable) ?? 0;
+      const y = yScale(series["probabilities"][variable.toString()]);
+      const coordinate = [x, y];
+      return coordinate;
+    });
+
+    const d = lineGenerator(allCoordinates);
+
+    if (!d) {
+      return <></>;
+    }
+
+    return <path key={i} d={d} stroke="skyblue" fill="none" />;
+  });
+
+  // Compute Axes
+  const allAxes = variables.map((variable, i) => {
+    const yScale = yScales[variable];
+    return (
+      <g key={i} transform={"translate(" + xScale(variable) + ",0)"}>
+        <AxisVertical yScale={yScale} pixelsPerTick={40} name={variable} />
+      </g>
+    );
+  });
+
+  return (
+    <svg width={width} height={height}>
+      <g
+        width={boundsWidth}
+        height={boundsHeight}
+        transform={`translate(${[MARGIN.left, MARGIN.top].join(",")})`}
+      >
+        {allLines}
+        {allAxes}
+      </g>
+    </svg>
+  );
+}
+
+export default ParallelCoordinatePlot;
